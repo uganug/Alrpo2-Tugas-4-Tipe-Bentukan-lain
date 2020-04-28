9428bdda4a20a5d8df22b33d53866b8f70fd3653
Nugraha Wanaspati <61569923+uganug@users.noreply.github.com>
2020-04-28 14:02:39 +0800

    Add files via upload

---
+++ Tugas 4 (no3).cpp
@@ -0,0 +1,17 @@
+//3. Buat program sederhana yang menggunakan typedef dalam pemrograman c++
+
+#include <iostream>
+using namespace std;
+	
+int main()
+{
+     typedef unsigned int umur;
+	
+     umur f = 20 , a = 19, n = 20;
+	
+     cout << "Umur Nugra = " << n<<endl;
+     cout << "Umur Firman = " << f<<endl;
+     cout << "Umur Andi = " << a;
+	
+      return 0;
+}
